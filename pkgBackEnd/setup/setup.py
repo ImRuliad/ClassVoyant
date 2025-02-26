62f859655c010002224bfedae728020e571539f1
ImRuliad <nicholaseovva@csus.edu>
2025-02-25 23:16:13 -0800

    webdriver optimizations

--- pkgBackEnd/setup/setup.py
+++ pkgBackEnd/setup/setup.py
@@ -1,13 +1,19 @@
 import logging
 from selenium import webdriver
 from selenium.common import WebDriverException
+from selenium.webdriver.chrome.options import Options
 
 
 def setup_driver():
 
     try:
+        chrome_options = Options()
+        chrome_options.page_load_strategy = "eager" #if some page elements don't load change to "none"
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-gpu")
         driver = webdriver.Chrome()
         return driver
+
     except FileNotFoundError:
         logging.error(f"A path to Chrome Webdriver was not found.")
         logging.error(f"Please ensure a Chrome WebDriver is downloaded and path is correctly set.")
