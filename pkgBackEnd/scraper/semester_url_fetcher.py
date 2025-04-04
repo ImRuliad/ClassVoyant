b5a23656321a86fbc5ec04e45485d12411d3ebff
Nicholas <nicholaseovva@csus.edu>
2025-04-03 17:00:12 -0700

    separate base url navigation logic

--- pkgBackEnd/scraper/semester_url_fetcher.py
+++ pkgBackEnd/scraper/semester_url_fetcher.py
@@ -10,25 +10,32 @@ class SemesterUrlFetcher:
         self.semester_urls = None
         self.list_of_sem_urls = []
 
-    def get_semester_urls(self):
+    def _navigate_to_url(self):
         if not self.webdriver:
             raise ValueError("Webdriver not set ")
         if not self.base_url:
             raise ValueError("Base URL not set")
-
         try:
             self.webdriver.get(self.base_url)
+        except Exception as e:
+            logging.error(f"Error navigating to base_url: {self.base_url} ---> {e}")
+            raise
+
+    def get_semester_urls(self):
+        try:
+            self._navigate_to_url()
             self.semester_urls = self.webdriver.find_elements(By.CSS_SELECTOR, '[aria-labelledby="article-head"] li a')
-            for html_element in self.semester_urls:
-                self._save_semester_urls(html_element)
+            self._save_semester_urls()
             return self.list_of_sem_urls
         except Exception as e:
             logging.error(f"Error getting semester URLs: {e}")
             return
 
-    def _save_semester_urls(self, html_element):
-        sem_url = html_element.get_attribute('href')
-        self.list_of_sem_urls.append(sem_url)
+    def _save_semester_urls(self):
+        for html_element in self.semester_urls:
+            sem_url = html_element.get_attribute('href')
+            self.list_of_sem_urls.append(sem_url)
+
 
 
 
