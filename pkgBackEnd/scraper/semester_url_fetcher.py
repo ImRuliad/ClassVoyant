ae041470db2b9b8ea2429da51cc202caef5dff97
Nicholas <nicholaseovva@csus.edu>
2025-04-03 17:18:25 -0700

    refactor semester url fetching

--- pkgBackEnd/scraper/semester_url_fetcher.py
+++ pkgBackEnd/scraper/semester_url_fetcher.py
@@ -7,8 +7,8 @@ class SemesterUrlFetcher:
     def __init__(self, webdriver, base_url):
         self.webdriver = webdriver
         self.base_url = base_url
-        self.semester_urls = None
         self.list_of_sem_urls = []
+        self.semester_link_elements = None
         self.semester_link_element = '[aria-labelledby="article-head"] li a'
 
     def _navigate_to_url(self):
@@ -24,25 +24,26 @@ class SemesterUrlFetcher:
 
     def _find_semester_link_elements(self):
         try:
-            return self.webdriver.find_elements(By.CSS_SELECTOR, self.semester_link_element)
+             self.semester_link_elements = self.webdriver.find_elements(By.CSS_SELECTOR, self.semester_link_element)
         except Exception as e:
             logging.error(f"Unable to find semester link element: {self.semester_link_element} ---> {e}")
+
+    def _extract_semester_urls_from_element(self):
+        for html_element in self.semester_link_elements:
+            semester_url = html_element.get_attribute('href')
+            self.list_of_sem_urls.append(semester_url)
 
 
     def get_semester_urls(self):
         try:
             self._navigate_to_url()
-            self.semester_urls = self._find_semester_link_elements()
-            self._save_semester_urls()
+            self._find_semester_link_elements()
+            self._extract_semester_urls_from_element()
             return self.list_of_sem_urls
         except Exception as e:
             logging.error(f"Error getting semester URLs: {e}")
             return
 
-    def _save_semester_urls(self):
-        for html_element in self.semester_urls:
-            sem_url = html_element.get_attribute('href')
-            self.list_of_sem_urls.append(sem_url)
 
 
 
