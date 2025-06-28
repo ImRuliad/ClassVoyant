0f5c14b0542d35dd1ba273375d3e70975b0e44c6
ImRuliad <nicholaseovva@csus.edu>
2025-06-28 12:56:34 -0700

    remove db

--- pkgBackEnd/database/models.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from create_db import Base
-
-
-class CourseName(Base):
-    pass
--- pkgBackEnd/setup/begin_scraper.py
+++ pkgBackEnd/setup/begin_scraper.py
@@ -6,20 +6,15 @@ from pkgBackEnd.utils.validators import validate_webdriver, validate_url
 
 
 def begin_fetch(driver):
-    #obtains base_url from .env file
     base_url = get_url_from_env.base_url()
 
-    #creates object for fetching semester urls. Returns a list of semester URLs.
-    #semester_urls[0] = fall-2025 semester_urls[1] = summer-2025 semester_urls[2] = spring-2025
     validate_webdriver(driver)
     validate_url(base_url)
 
     semester_url_fetcher = SemesterUrlFetcher(driver, base_url)
     semester_urls: list = semester_url_fetcher.get_semester_urls()
 
-    #creates object for fetching major URLs based on their correlated semester URL.
-    #returns a dictionary of major URLs.
-    #find a way to use validate_url before making the call directly below.
+    #semester_urls[0] = fall-2025 semester_urls[1] = summer-2025 semester_urls[2] = spring-2025
     major_url_fetcher = MajorUrlFetcher(driver, semester_urls[2])
     major_urls: dict = major_url_fetcher.get_major_urls()
 
