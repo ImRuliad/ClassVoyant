a867113c95ed87cb46fc9f376af86d7a99a30f13
ImRuliad <nicholaseovva@csus.edu>
2025-06-30 20:48:58 -0700

    create model for courses

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Course(models.Model):
+    course_id = models.CharField(max_length=10, primary_key=True)
+    course_title = models.CharField(max_length=200)
+    units = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.course_id}: {self.course_title}"
