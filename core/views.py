2dd60f1445e575e1635b3e5ef933a8b57aadb57b
ImRuliad <nicholaseovva@csus.edu>
2025-07-01 01:31:58 -0700

    create view for general course data

--- core/views.py
+++ core/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Course
 # Create your views here.
+
+def courses(request):
+    courses = Course.objects.all()
+    return render(request, 'core/courses.html', {'courses': courses})
