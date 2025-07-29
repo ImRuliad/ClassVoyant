d54e0a9c84538626951735ff8f512e7167ccedcd
ImRuliad <nicholaseovva@csus.edu>
2025-07-29 14:28:32 -0700

    start frontend

--- pkgFrontEnd/src/App.tsx
+++ pkgFrontEnd/src/App.tsx
@@ -1,37 +1,31 @@
 import './App.css'
 import {Spotlight} from './components/ui/spotlight-new'
-import { FlipWords } from './components/ui/flip-words'
-import { HoverBorderGradient } from './components/ui/hover-border-gradient'
+import {FlipWords} from './components/ui/flip-words'
+import {HoverBorderGradient} from './components/ui/hover-border-gradient'
 
 function App() {
   const words = ["faster.", "easier.", "stress free.", "with AI."];
+  const title = "ClassVoyant";
+  const subtitle = "Pick your classes, get your schedule";
+  const buttonText = "Find your classes";
+  const buttonLink = "http://localhost:8000/api/courses/";
 
   return (
-    <main className="relative min-h-screen w-full bg-black/[0.96] antialiased bg-grid-white/[0.02] overflow-hidden">
-      <Spotlight />
-      <section className="relative z-10 flex flex-col items-center justify-center min-h-[40rem] max-w-7xl mx-auto p-4 pt-20 md:pt-0 rounded-md">
-        <header>
-          <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
-            Welcome to ClassVoyant <br />
-          </h1>
-        </header>
-        <p className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto">
-          Less stress, better classes, fewer tabs. Find your course schedule <FlipWords words={words} />
-        </p>
-        <div className="mt-8 flex justify-center">
-          <HoverBorderGradient
-            containerClassName="rounded-full"
-            as="a"
-            // TODO: change to frontend url
-            href="http://localhost:8000/api/courses/"
-            className="dark:bg-black bg-white text-black dark:text-white flex items-center space-x-2"
-            aria-label="Find your classes"
-          >
-            <span>Find your classes</span>
+    <main className="relative min-h-screen w-full bg-black/[0.96] antialiased bg-grid-white/[0.02] overflow-hidden flex items-center justify-center">
+      <Spotlight></Spotlight>
+        <div className="relative z-10">
+          <h1 className="text-4xl text-white text-center">{title}</h1>
+          <h2 className="text-2xl text-white text-center">{subtitle}<FlipWords words={words}/></h2>
+          <div className="flex justify-center mt-8">
+          <HoverBorderGradient>
+            <button>
+              <a href={buttonLink}>{buttonText}</a>
+            </button>
           </HoverBorderGradient>
-        </div>
-      </section>
+          </div>
+      </div>
     </main>
+    
   );
 }
 
